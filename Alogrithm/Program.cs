6b78b4ee7303af5d9318c1b521450f768f1e5789
zyh <72659288+zhang666zyh@users.noreply.github.com>
2021-09-18 14:33:51 +0800

    Add files via upload

---
+++ Alogrithm/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fm = 0;
+            double sum = 0.0;
+            for(fm = 1;fm <= 100; fm++)
+            {
+                if(fm % 2 == 0)
+                {
+                    sum -= 1.0 / fm;
+                }
+                else
+                {
+                    sum += 1.0 / fm;
+                }
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+
+
+}
